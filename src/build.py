db70253df72ee714e70b304b1946a00e61d83cfb
Guillaume Kesteman <guillaume.kesteman@enigma-school.com>
2024-04-18 16:13:10 +0200

    feat: add comments and set into function result print

--- src/build.py
+++ src/build.py
@@ -1,3 +1,6 @@
 from sentence_transformers import SentenceTransformer
 
+# download sbert model during docker build
+# https://www.sbert.net/docs/pretrained_models.html
+# https://huggingface.co/sentence-transformers/all-mpnet-base-v2
 SentenceTransformer("all-mpnet-base-v2")
