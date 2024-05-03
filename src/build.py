1d55c7ea4ca59953921f6d068cc543adea6b575b
Guillaume Kesteman <guillaume.kesteman@enigma-school.com>
2024-05-03 18:22:46 +0200

    feat: download wikipedia dataset on build

--- src/build.py
+++ src/build.py
@@ -1,4 +1,5 @@
 import warnings
+from datasets import load_dataset
 from sentence_transformers import SentenceTransformer
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
@@ -7,4 +8,10 @@ warnings.simplefilter(action="ignore", category=FutureWarning)
 # https://www.sbert.net/docs/pretrained_models.html
 # https://huggingface.co/sentence-transformers/all-mpnet-base-v2
 SentenceTransformer("all-mpnet-base-v2")
+
+# https://pypi.org/project/bert-extractive-summarizer/#use-sbert
 SentenceTransformer("paraphrase-MiniLM-L6-v2")
+
+# import wikipedia dataset
+# source : https://huggingface.co/datasets/wikipedia
+load_dataset("wikipedia", "20220301.fr", trust_remote_code=True)
